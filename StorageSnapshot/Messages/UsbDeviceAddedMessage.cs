1601bf4474c3d6ff81cb9f8305d077a010cc50a3
Derek Gooding <djgooding@live.com>
2024-02-12 10:07:19 -0500

    Removed Unnecessary IMessage implementation

--- StorageSnapshot/Messages/UsbDeviceAddedMessage.cs
+++ StorageSnapshot/Messages/UsbDeviceAddedMessage.cs
@@ -1,36 +1,9 @@
-﻿using CommunityToolkit.Mvvm.Messaging;
+﻿namespace StorageSnapshot.Messages;
 
-namespace StorageSnapshot.Messages;
-
-public sealed class UsbDeviceAddedMessage : IMessenger
+public sealed class UsbDeviceAddedMessage
 {
     public required string DeviceId
     {
         get; set;
     }
-
-    public void Cleanup() => throw new NotImplementedException();
-
-    public bool IsRegistered<TMessage, TToken>(object recipient, TToken token)
-        where TMessage : class
-        where TToken : IEquatable<TToken> => throw new NotImplementedException();
-
-    public void Register<TRecipient, TMessage, TToken>(TRecipient recipient, TToken token, MessageHandler<TRecipient, TMessage> handler)
-        where TRecipient : class
-        where TMessage : class
-        where TToken : IEquatable<TToken> => throw new NotImplementedException();
-
-    public void Reset() => throw new NotImplementedException();
-
-    public TMessage Send<TMessage, TToken>(TMessage message, TToken token)
-        where TMessage : class
-        where TToken : IEquatable<TToken> => throw new NotImplementedException();
-
-    public void Unregister<TMessage, TToken>(object recipient, TToken token)
-        where TMessage : class
-        where TToken : IEquatable<TToken> => throw new NotImplementedException();
-
-    public void UnregisterAll(object recipient) => throw new NotImplementedException();
-
-    public void UnregisterAll<TToken>(object recipient, TToken token) where TToken : IEquatable<TToken> => throw new NotImplementedException();
 }
